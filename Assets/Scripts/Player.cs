a3dafcae256e6fa2ebbe1dcecf1d73b65c08f190
Gabirel77 <gabriel.sm@estudante.iftm.edu.br>
2025-07-31 13:01:50 -0300

    feat: implementar movimentação básica do player com Rigidbody e Collider

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
-    float speed = 3.5f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+{   
+    public float speed = 5.5f;
+    private Vector2 movement;
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private float jumpForce = 6;
+    
     // Update is called once per frame
     void Update()
     {
-        
+        float moveHorizontally = Input.GetAxis("Horizontal");
+        movement.x = moveHorizontally * speed * Time.deltaTime;
+        transform.Translate(movement);
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        //adiciona impulso ao rigidbody pra pular
+        rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 }
