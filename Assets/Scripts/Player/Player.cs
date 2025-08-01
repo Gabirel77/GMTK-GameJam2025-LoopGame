e350106846caaf298e6cfe5c5f3f22c30b10458b
Gabirel77 <gabriel.sm@estudante.iftm.edu.br>
2025-07-31 21:49:46 -0300

    feat: add scene transition system with fade and persistent background music

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{   
+    public float speed = 3.35f;
+    private Vector2 movement;
+    [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private float jumpForce = 6;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontally = Input.GetAxis("Horizontal");
+        movement.x = moveHorizontally * speed * Time.deltaTime;
+        transform.Translate(movement);
+        
+        if (Input.GetButtonDown("Jump")&& GetIsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    private bool GetIsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.8f, LayerMask.GetMask("Ground"));
+    }
+
+    private void Jump()
+    {
+        //adiciona impulso ao rigidbody pra pular
+        rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
